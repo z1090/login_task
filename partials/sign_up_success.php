ba40d06dc5cbf707f36e2c94de1af928d0f79392
Ian Wildsmith <ian.wildsmith@gmail.com>
2019-02-14 15:24:57 +0000

    sign up success message

--- partials/sign_up_success.php
+++ partials/sign_up_success.php
@@ -1,26 +1,3 @@
 
 <h1>Sign Up</h1>
-<p><?echo$errMessage?></p>
-<form action="sign_up.php" method="POST">
-    <section class="form-group">
-        <label for="first_name">First Name</label>
-        <input type="text" name="first_name" id="first_name" />
-    </section>
-    <section class="form-group">
-        <label for="last_name">Last Name</label>
-        <input type="text" name="last_name" id="last_name" />
-    </section>
-    <section class="form-group">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" />
-    </section>
-    <section class="form-group">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" />
-    </section>
-    <section class="form-group">
-        <label for="password-confirm">Confirm Password</label>
-        <input type="password" name="password-confirm" id="password-confirm" />
-    </section>
-    <input type="submit" name="action" value="Sign up" />
-</form>
+<p>That's gurt lush. We've sent you an email to validate your account.</p>
--- sign_up.php
+++ sign_up.php
@@ -2,23 +2,27 @@
 
 <?PHP
 
-
     
     if($_POST){
+        $errors = false;
         foreach($_POST as $key => $value) {
             if(empty($value)){
                 $errMessage = "Missing fields";
+                $errors = true;
                 include "./partials/sign_up_form.php";
                 break;
             };
         };
         if($_POST["password"] !== $_POST["password-confirm"]){
+            $errors = true;
             $errMessage = "Passwords don't match.";
             include "./partials/sign_up_form.php";
         };
 
-        header("Location: ./sign_up_form.php?success=true");
-        header("Location: ./sign_up_success.php");
+        if(!$errors){
+            // header("Location: ./sign_up_form.php?success=true");
+            header("Location: ./partials/sign_up_success.php");
+        };
 
 
     } else {
